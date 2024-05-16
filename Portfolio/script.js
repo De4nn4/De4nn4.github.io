cb8dd01ceadf64c37e3d595d1d5eb0d6413c38c7
De4nn4 <164976252+De4nn4@users.noreply.github.com>
2024-05-16 17:30:33 +1000

    Add files via upload

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,6 +1,28 @@
+/* carousel */
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
-  // options
   cellAlign: 'left',
-  contain: true
+  contain: true , 
+  wrapAround: true
 });
+
+/* list for project */
+function filterCategory(filterBy, buttonClicked){
+  let itemsToFilter = Array.from(document.getElementsByClassName("RecipeBox"));
+  for(let listItem of itemsToFilter){
+    if(listItem.classList.contains(filterBy)){
+      listItem.style.display = "block";
+    } else {
+      listItem.style.display = "none";
+    }
+  }
+
+  let filterButtonsArray = Array.from(document.getElementsByClassName("FilterButton"));
+  for(let filterButton of filterButtonsArray){
+    if(filterButton === buttonClicked){
+      filterButton.classList.add("SelectedFilter");
+    } else {
+      filterButton.classList.remove("SelectedFilter");
+    }
+  }
+}
